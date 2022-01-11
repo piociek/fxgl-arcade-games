0e8c08e43f41ad9b3a469d91296e3acd8de40a2d
Piociek <pio.michalski@gmail.com>
2022-01-11 20:06:15 +0100

    * cleanUp * physics fixed

---
+++ pong/src/main/java/pong/PongEntityFactory.java
@@ -0,0 +1,79 @@
+package pong;
+
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.EntityFactory;
+import com.almasb.fxgl.entity.SpawnData;
+import com.almasb.fxgl.entity.Spawns;
+import com.almasb.fxgl.physics.BoundingShape;
+import com.almasb.fxgl.physics.HitBox;
+import com.almasb.fxgl.physics.PhysicsComponent;
+import com.almasb.fxgl.physics.box2d.dynamics.BodyType;
+import javafx.scene.shape.Rectangle;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
+import static pong.PongConstants.*;
+import static pong.PongEntityType.*;
+
+public class PongEntityFactory implements EntityFactory {
+
+    @Spawns("paddle")
+    public Entity paddle(SpawnData spawnData) {
+        PhysicsComponent physicsComponent = new PhysicsComponent();
+        physicsComponent.setBodyType(BodyType.KINEMATIC);
+
+        return entityBuilder(spawnData)
+                .type(PADDLE)
+                .bbox(new HitBox(BoundingShape.box(PADDLE_WIDTH, PADDLE_HEIGHT)))
+                .viewWithBBox(new Rectangle(PADDLE_WIDTH, PADDLE_HEIGHT))
+                .collidable()
+                .with(physicsComponent)
+                .build();
+    }
+
+    @Spawns("ball")
+    public Entity ball(SpawnData spawnData) {
+        PhysicsComponent physicsComponent = new PhysicsComponent();
+        physicsComponent.setBodyType(BodyType.DYNAMIC);
+
+        return entityBuilder(spawnData)
+                .type(BALL)
+                .bbox(new HitBox(BoundingShape.box(BALL_SIZE, BALL_SIZE)))
+                .viewWithBBox(new Rectangle(BALL_SIZE, BALL_SIZE))
+                .collidable()
+                .with(physicsComponent)
+                .build();
+    }
+
+    @Spawns("wall")
+    public Entity wall(SpawnData spawnData) {
+        return entityBuilder()
+                .type(WALL)
+                .at(-BALL_SIZE, spawnData.getY())
+                .bbox(new HitBox(BoundingShape.box(getAppWidth() + (2 * BALL_SIZE), 0)))
+                .collidable()
+                .with(new PhysicsComponent())
+                .build();
+    }
+
+    @Spawns("void_left")
+    public Entity voidLeft(SpawnData spawnData) {
+        return entityBuilder()
+                .type(VOID_LEFT)
+                .at(-BALL_SIZE, 0)
+                .bbox(new HitBox(BoundingShape.box(0, getAppHeight())))
+                .collidable()
+                .with(new PhysicsComponent())
+                .buildAndAttach();
+    }
+
+    @Spawns("void_right")
+    public Entity voidRight(SpawnData spawnData) {
+        return entityBuilder()
+                .type(VOID_RIGHT)
+                .at(getAppWidth() + BALL_SIZE, 0)
+                .bbox(new HitBox(BoundingShape.box(0, getAppHeight())))
+                .collidable()
+                .with(new PhysicsComponent())
+                .buildAndAttach();
+    }
+}
--- pong/src/main/java/pong/PongGame.java
+++ pong/src/main/java/pong/PongGame.java
@@ -4,30 +4,22 @@ import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.logging.Logger;
-import com.almasb.fxgl.physics.BoundingShape;
-import com.almasb.fxgl.physics.HitBox;
 import com.almasb.fxgl.physics.PhysicsComponent;
-import com.almasb.fxgl.physics.box2d.dynamics.BodyType;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
 import java.util.Map;
+import java.util.Objects;
 
 import static com.almasb.fxgl.dsl.FXGL.*;
-import static pong.EntityTypes.*;
+import static pong.PongConstants.*;
+import static pong.PongEntityType.*;
 
 public class PongGame extends GameApplication {
 
     private static final Logger LOGGER = Logger.get(PongGame.class);
-
-    private static final int PADDLE_WIDTH = 30;
-    private static final int PADDLE_HEIGHT = 100;
-    private static final int BALL_SIZE = 20;
-    private static final int PADDLE_SPEED = 300;
-    private static final int BALL_SPEED = 300;
-
+    private final PongEntityFactory pongEntityFactory = new PongEntityFactory();
     private Entity paddle1;
     private Entity paddle2;
     private Entity ball;
@@ -53,97 +45,52 @@ public class PongGame extends GameApplication {
 
     @Override
     protected void initGameVars(Map<String, Object> vars) {
-        vars.put("score1", 0);
-        vars.put("score2", 0);
+        vars.put(SCORE_1, 0);
+        vars.put(SCORE_2, 0);
     }
 
     @Override
     protected void initGame() {
-        spawnWall(0);
-        spawnWall(getAppHeight());
-        spawnVoid_left();
-        spawnVoid_right();
+        getGameWorld().addEntityFactory(pongEntityFactory);
 
-        paddle1 = spawnBat(0, (getAppHeight() / 2) - (PADDLE_HEIGHT / 2));
-        paddle2 = spawnBat(getAppWidth() - PADDLE_WIDTH, (getAppHeight() / 2) - (PADDLE_HEIGHT / 2));
-        ball = spawnBall((getAppWidth() / 2) - (BALL_SIZE / 2), (getAppHeight() / 2) - (BALL_SIZE / 2));
+        spawn(ENTITY_WALL, 0, 0);
+        spawn(ENTITY_WALL, 0, getAppHeight());
+        spawn(ENTITY_VOID_LEFT);
+        spawn(ENTITY_VOID_RIGHT);
+
+        paddle1 = spawn(ENTITY_PADDLE,
+                0,
+                getAppHeight() / 2 - PADDLE_HEIGHT / 2);
+        paddle2 = spawn(ENTITY_PADDLE,
+                getAppWidth() - PADDLE_WIDTH,
+                getAppHeight() / 2 - PADDLE_HEIGHT / 2);
+        spawnNewBall(Direction.RIGHT);
 
         ball.getComponent(PhysicsComponent.class).setVelocityX(BALL_SPEED);
     }
 
-    private Entity spawnBat(double x, double y) {
-        PhysicsComponent physicsComponent = new PhysicsComponent();
-        physicsComponent.setBodyType(BodyType.DYNAMIC);
-
-        return entityBuilder()
-                .type(PADDLE)
-                .at(x, y)
-                .bbox(new HitBox(BoundingShape.box(PADDLE_WIDTH, PADDLE_HEIGHT)))
-                .viewWithBBox(new Rectangle(PADDLE_WIDTH, PADDLE_HEIGHT))
-                .collidable()
-                .with(physicsComponent)
-                .buildAndAttach();
-    }
-
-    private Entity spawnBall(double x, double y) {
-        PhysicsComponent physicsComponent = new PhysicsComponent();
-        physicsComponent.setBodyType(BodyType.DYNAMIC);
-
-        return entityBuilder()
-                .type(BALL)
-                .at(x, y)
-                .bbox(new HitBox(BoundingShape.box(BALL_SIZE, BALL_SIZE)))
-                .viewWithBBox(new Rectangle(BALL_SIZE, BALL_SIZE))
-                .collidable()
-                .with(physicsComponent)
-                .buildAndAttach();
-    }
-
-    private Entity spawnWall(double y) {
-        return entityBuilder()
-                .type(WALL)
-                .at(-BALL_SIZE, y)
-                .bbox(new HitBox(BoundingShape.box(getAppWidth() + (2 * BALL_SIZE), 0)))
-                .collidable()
-                .with(new PhysicsComponent())
-                .buildAndAttach();
-    }
-
-    private Entity spawnVoid_left() {
-        return entityBuilder()
-                .type(VOID_LEFT)
-                .at(-BALL_SIZE, 0)
-                .bbox(new HitBox(BoundingShape.box(0, getAppHeight())))
-                .collidable()
-                .with(new PhysicsComponent())
-                .buildAndAttach();
-    }
-
-    private Entity spawnVoid_right() {
-        return entityBuilder()
-                .type(VOID_RIGHT)
-                .at(getAppWidth() + BALL_SIZE, 0)
-                .bbox(new HitBox(BoundingShape.box(0, getAppHeight())))
-                .collidable()
-                .with(new PhysicsComponent())
-                .buildAndAttach();
-    }
-
     @Override
     protected void initUI() {
-        Text textScore1 = getUIFactoryService().newText("", Color.BLACK, 22);
-        Text textScore2 = getUIFactoryService().newText("", Color.BLACK, 22);
+        Text textScoreTop = getUIFactoryService().newText("Score", Color.BLACK, 25);
+        textScoreTop.setTranslateX(getAppWidth() / 2 - 25);
+        textScoreTop.setTranslateY(100);
 
-        textScore1.setTranslateX(10);
-        textScore1.setTranslateY(50);
+        Text textScoreBottom = getUIFactoryService().newText(":", Color.BLACK, 25);
+        textScoreBottom.setTranslateX(getAppWidth() / 2);
+        textScoreBottom.setTranslateY(130);
 
-        textScore2.setTranslateX(getAppWidth() - 30);
-        textScore2.setTranslateY(50);
+        Text textScore1 = getUIFactoryService().newText("", Color.BLACK, 25);
+        textScore1.setTranslateX(getAppWidth() / 2 - 25);
+        textScore1.setTranslateY(130);
 
-        textScore1.textProperty().bind(getWorldProperties().intProperty("score1").asString());
-        textScore2.textProperty().bind(getWorldProperties().intProperty("score2").asString());
+        Text textScore2 = getUIFactoryService().newText("", Color.BLACK, 25);
+        textScore2.setTranslateX(getAppWidth() / 2 + 25);
+        textScore2.setTranslateY(130);
 
-        getGameScene().addUINodes(textScore1, textScore2);
+        textScore1.textProperty().bind(getWorldProperties().intProperty(SCORE_1).asString());
+        textScore2.textProperty().bind(getWorldProperties().intProperty(SCORE_2).asString());
+
+        getGameScene().addUINodes(textScoreTop, textScoreBottom, textScore1, textScore2);
     }
 
     @Override
@@ -162,40 +109,37 @@ public class PongGame extends GameApplication {
                         LOGGER.fatal("Ball velocityX == 0");
                     }
                     ball.getComponent(PhysicsComponent.class)
-                            .setVelocityY(ball.getComponent(PhysicsComponent.class).getVelocityY() + paddle.getComponent(PhysicsComponent.class).getVelocityY());
+                            .setVelocityY(ball.getComponent(PhysicsComponent.class)
+                                    .getVelocityY() + paddle.getComponent(PhysicsComponent.class).getVelocityY());
                 }
         );
         onCollisionBegin(BALL, WALL, (ball, wall) ->
-                ball.getComponent(PhysicsComponent.class).setVelocityY(ball.getComponent(PhysicsComponent.class).getVelocityY() * -1));
+                ball.getComponent(PhysicsComponent.class)
+                        .setVelocityY(ball.getComponent(PhysicsComponent.class)
+                                .getVelocityY() * -1));
 
         onCollision(BALL, VOID_LEFT, (ball, voidLeft) -> {
-            getWorldProperties().increment("score2", +1);
+            getWorldProperties().increment(SCORE_2, +1);
             spawnNewBall(Direction.RIGHT);
         });
 
         onCollision(BALL, VOID_RIGHT, (ball, voidRight) -> {
-            getWorldProperties().increment("score1", +1);
+            getWorldProperties().increment(SCORE_1, +1);
             spawnNewBall(Direction.LEFT);
+        });
+
+        onCollision(PADDLE, WALL, (paddle, wall) -> {
+            paddle.getComponent(PhysicsComponent.class).setVelocityY(0);
         });
     }
 
     private void spawnNewBall(Direction direction) {
-        ball.removeFromWorld();
-        ball = spawnBall((getAppWidth() / 2) - (BALL_SIZE / 2), (getAppHeight() / 2) - (BALL_SIZE / 2));
+        if (Objects.nonNull(ball)) {
+            ball.removeFromWorld();
+        }
+        ball = spawn(ENTITY_BALL,
+                getAppWidth() / 2 - BALL_SIZE / 2,
+                getAppHeight() / 2 - BALL_SIZE / 2);
         ball.getComponent(PhysicsComponent.class).setVelocityX(BALL_SPEED * direction.value);
     }
-}
-
-enum EntityTypes {
-    BALL, PADDLE, WALL, VOID_LEFT, VOID_RIGHT
-}
-
-enum Direction {
-    LEFT(-1), RIGHT(1);
-
-    Direction(int value) {
-        this.value = value;
-    }
-
-    int value;
 }
