4372523dd50325faf183098ad6bacbdc8d249878
piociek <pio.michalski@gmail.com>
2022-01-21 16:31:00 +0100

    Space Invaders game updated - particle explosions added - player reset updated - entity sizes updated - gitignore updated

--- space-invaders/src/main/java/com/piociek/fxgl/spaceinvaders/SpaceInvadersConstants.java
+++ space-invaders/src/main/java/com/piociek/fxgl/spaceinvaders/SpaceInvadersConstants.java
@@ -17,26 +17,28 @@ public abstract class SpaceInvadersConstants {
     public static final String UI_LIVES = "Lives: ";
     public static final String UI_HIGH_SCORE = "HighScore: ";
 
+    public static final String ENTITY_BACKGROUND = "background";
+    public static final String ENTITY_SCREEN_END = "screenEnd";
     public static final String ENTITY_PLAYER = "player";
     public static final String ENTITY_ENEMY = "enemy";
     public static final String ENTITY_UFO = "ufo";
     public static final String ENTITY_BULLET = "bullet";
     public static final String ENTITY_BOMB = "bomb";
-    public static final String ENTITY_SCREEN_END = "screenEnd";
+    public static final String ENTITY_EXPLOSION = "explosion";
 
-    public static final Double PLAYER_WIDTH = 30.0;
-    public static final Double PLAYER_HEIGHT = 35.0;
+    public static final Double PLAYER_WIDTH = 32.0;
+    public static final Double PLAYER_HEIGHT = 32.0;
     public static final Double PLAYER_SPEED = 4.0;
 
     public static final Double BULLET_WIDTH = 15.0;
     public static final Double BULLET_HEIGHT = 2.0;
     public static final Double BULLET_SPEED = 450.0;
 
-    public static final Double ENEMY_WIDTH = 35.0;
-    public static final Double ENEMY_HEIGHT = 35.0;
+    public static final Double ENEMY_WIDTH = 32.0;
+    public static final Double ENEMY_HEIGHT = 32.0;
     public static final Integer ENEMY_ROW_COUNT = 5;
     public static final Integer ENEMY_COUNT_PER_ROW = 12;
-    public static final Integer ENEMY_SPACING = 15;
+    public static final Integer ENEMY_SPACING = 16;
 
     public static final Double ENEMY_SPEED = 0.5;
     public static final Double ENEMY_SPEED_INCREASE = 0.1;
@@ -55,8 +57,8 @@ public abstract class SpaceInvadersConstants {
     public static final Double BOMB_SPEED = 350.0;
     public static final Double BOMB_SPAWN_INTERVAL = 1.8;
 
-    public static final Double UFO_WIDTH = 35.0;
-    public static final Double UFO_HEIGHT = 35.0;
+    public static final Double UFO_WIDTH = 64.0;
+    public static final Double UFO_HEIGHT = 32.0;
     public static final Double UFO_SPEED = 75.0;
     public static final Integer UFO_SCORE = 150;
     public static final Double UFO_SPAWN_INTERVAL = 25.0;
